a104386c32501669d3bcb4cab0b80abf6ee99a17
1Door <89485167+1Door@users.noreply.github.com>
2022-09-14 16:12:24 -0500

    comments

--- src/lab/Trip.java
+++ src/lab/Trip.java
@@ -22,7 +22,7 @@ package lab;
  */
 
 
-
+ //do I have to comment this?
 public class Trip {
 
 	public static void main(String[] args) {
@@ -31,32 +31,32 @@ public class Trip {
 		 * Constructor assumes this is the start of a trip
 		 * All variables should be initialized accordingly
 		 */
-		RoadTripLog trip = new RoadTripLog("Maryland 2022");
+		RoadTripLog trip = new RoadTripLog("Maryland 2022"); //name
 		
-		trip.addDistance(358.4);
-		trip.addFuel(18.4);
+		trip.addDistance(358.4); //adds  to the distance
+		trip.addFuel(18.4); //adds to the  fuel
 		
 		System.out.println("Stop #1");
-		System.out.println("   " + trip.getTotalDistance());
+		System.out.println("   " + trip.getTotalDistance()); //prints stuff
 		System.out.println("   " + trip.getTotalFuel());
 		System.out.println("   " + trip.getMPG() + "\n");
 		
 		
-		trip.addDistance(215.8);
-		trip.addFuel(12.5);
+		trip.addDistance(215.8); //adds to  distance
+		trip.addFuel(12.5); //adds to fuel
 		
 		System.out.println("Stop #2");
-		System.out.println("   " + trip.getTotalDistance());
+		System.out.println("   " + trip.getTotalDistance()); //prints stuff
 		System.out.println("   " + trip.getTotalFuel());
 		System.out.println("   " + trip.getMPG() + "\n");
 		
-		trip.addDistance(312.9);
-		trip.addFuel(16.8);
+		trip.addDistance(312.9); //adds to distance
+		trip.addFuel(16.8); //adds to  fuel
 		
-		trip.addDistance(108.6);
-		trip.addFuel(7.5);
+		trip.addDistance(108.6); //adds to distance
+		trip.addFuel(7.5); //adds to fuel
 		
-		System.out.println(trip);
+		System.out.println(trip); //prints the  results
 		
 	}
 	
